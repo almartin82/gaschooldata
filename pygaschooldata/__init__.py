1457b3eaa1e93e30249fb72a551a1c94798f87cd
almartin82 <almartin@gmail.com>
2025-12-31 20:11:18 -0500

    Add Python wrapper package

---
+++ pygaschooldata/__init__.py
@@ -0,0 +1,16 @@
+"""
+pygaschooldata - Python wrapper for Georgia school enrollment data.
+
+Thin rpy2 wrapper around the gaschooldata R package.
+Returns pandas DataFrames.
+"""
+
+from .core import (
+    fetch_enr,
+    fetch_enr_multi,
+    tidy_enr,
+    get_available_years,
+)
+
+__version__ = "0.1.0"
+__all__ = ["fetch_enr", "fetch_enr_multi", "tidy_enr", "get_available_years"]
