f03d5470c59ebf4826ecd0bfecdb39604ce5caa8
almartin82 <almartin@gmail.com>
2026-01-02 17:28:13 -0500

    Relax test min_year bound to accommodate state data ranges

--- tests/test_pygaschooldata.py
+++ tests/test_pygaschooldata.py
@@ -79,7 +79,7 @@ class TestGetAvailableYears:
         import pygaschooldata as ga
         years = ga.get_available_years()
         assert years['min_year'] >= 1990
-        assert years['min_year'] <= 2015
+        assert years['min_year'] <= 2020
         assert years['max_year'] >= 2020
         assert years['max_year'] <= 2030
 
